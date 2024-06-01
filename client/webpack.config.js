dcd622d5f2c62fd96cb610a45cf65b4b79288d5d
Max Bonetti <maxwell.bonetti@gmail.com>
2024-06-01 12:40:39 -0500

    finish and save. added all functionality to install.js, sw.js, and index.js on client side

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,67 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const path = require('path');
 
 module.exports = () => {
   return {
     mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
+    entry: './src/js/index.js',
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E'
+      }),
+      new WebpackPwaManifest({
+        name:'Just Another Text Editor',
+        short_name:'JATE',
+        description:'A simple text editor that works offline. Takes notes in JavaScript Syntax!',
+        fingerprints: false,
+        inject: true,
+        background_color:'#ffffff',
+        crossorigin: 'use-credentials',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+            purpose: 'any maskable'
+          },
+        ],
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      })
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [{ loader: 'file-loader' }]
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
